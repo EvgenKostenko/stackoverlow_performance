e1895e3f218cf2ef06386c101be9f059a8d38234
Evgen Kostenko <evgeniy.kostenko@mev.com>
2016-11-28 16:35:21 +0200

    Fix main

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"github.com/EvgenKostenko/stackoverlow_performance/api"
 	"github.com/EvgenKostenko/stackoverlow_performance/config"
 	"github.com/kataras/iris"
 )
 
 func main() {
-	fmt.Printf("config: %#v", config.Config)
 	iris.Get("/topusers/:word", api.GetTopUsersByWord)
 	iris.Get("/toptags/:userid", api.GetTopUsersTags)
 	iris.Listen(config.Config.API.Host)
